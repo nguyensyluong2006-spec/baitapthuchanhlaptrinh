84b32e882e832392dfaa829ea4d034962ff9ae64
nguyensyluong2006-spec <nguyensyluong2006@gmail.com>
2025-12-01 07:58:10 +0700

    Add print statements for author and ID

--- Bai4_1.py
+++ Bai4_1.py
@@ -1,3 +1,7 @@
+print("nguyễn sỹ lương")
+print("245752021610139")
+print("##############################")
+#######################################
 # Bài 1: Nhập chuỗi S và in ra từng ký tự của S, mỗi ký tự trên một dòng
 S = input('Nhập chuỗi: ')
 for ky_tu in S:
