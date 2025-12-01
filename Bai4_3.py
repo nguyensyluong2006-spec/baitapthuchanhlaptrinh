727664c902fec24985cc28a5216852bb9cda1b53
nguyensyluong2006-spec <nguyensyluong2006@gmail.com>
2025-12-01 08:12:53 +0700

    Add print statements for user information

--- Bai4_3.py
+++ Bai4_3.py
@@ -1,3 +1,7 @@
+print("nguyễn sỹ lương")
+print("245752021610139")
+print("##############################")
+#######################################
 # Bài 3: Chỉ in ra các ký tự có dạng IN HOA
 S = input('Nhập chuỗi: ')
 for ky_tu in S:
