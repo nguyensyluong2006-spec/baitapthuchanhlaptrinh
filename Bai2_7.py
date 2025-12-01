effcd19f5ba5c21113a2d2fbfebbc2ba566b3524
nguyensyluong2006-spec <nguyensyluong2006@gmail.com>
2025-12-01 07:55:01 +0700

    Add print statements and dictionary creation

--- Bai2_7.py
+++ Bai2_7.py
@@ -1,3 +1,7 @@
+print("nguyễn sỹ lương")
+print("245752021610139")
+print("#############################")
+######################################
 n=int(input("nhap vao mot so nguyen:"))
 d=dict()
 for i in range (1,n+1):
