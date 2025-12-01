daa611314670401b8de5ae81c724d89faf5817e4
nguyensyluong2006-spec <nguyensyluong2006@gmail.com>
2025-12-01 07:55:17 +0700

    Add print statements for user information

--- Bai2_8.py
+++ Bai2_8.py
@@ -1,3 +1,7 @@
+print("nguyễn sỹ lương")
+print("245752021610139")
+print("#############################")
+######################################
 a, b = 1, 2
 total = 0
 print("Fibonacci sequence less than 4,000,000:")
