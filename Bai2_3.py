082e8d3c66c91dd6fcd9f257e403ab02b3f3795d
nguyensyluong2006-spec <nguyensyluong2006@gmail.com>
2025-12-01 07:53:20 +0700

    Add print statements for user information

--- Bai2_3.py
+++ Bai2_3.py
@@ -1,3 +1,7 @@
+print("nguyễn sỹ lương")
+print("245752021610139")
+print("#############################")
+######################################
 n=int(input("enter a number:"))
 if n% 2==0:
 	print("EVEN NUMBER")
