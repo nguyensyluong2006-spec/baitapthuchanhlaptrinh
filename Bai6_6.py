c9a63145cae4757a758cb02818dadf0eb155709e
nguyensyluong2006-spec <nguyensyluong2006@gmail.com>
2025-12-01 08:28:53 +0700

    Update Bai6_6.py

--- Bai6_6.py
+++ Bai6_6.py
@@ -1,3 +1,7 @@
+print("nguyễn sỹ lương")
+print("245752021610139")
+print("##############################")
+#######################################
 class StringProcessor:
     def __init__(self):
         self.input_string = ""
