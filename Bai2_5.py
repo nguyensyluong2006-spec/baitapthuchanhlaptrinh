2e33a437727f84604423df055254a86b8ba1192e
nguyensyluong2006-spec <nguyensyluong2006@gmail.com>
2025-12-01 07:54:17 +0700

    Add print statements and modify input handling

--- Bai2_5.py
+++ Bai2_5.py
@@ -1,3 +1,7 @@
+print("nguyễn sỹ lương")
+print("245752021610139")
+print("#############################")
+######################################
 n = int(input("enter a number (n>0):"))
 while n >= 0:
     print(n)
